1d1e8551bce5c548f045f7556b9aae2617133e00
Kevin <kmaitland9@gmail.com>
2015-10-21 04:18:13 -0400

    Updated router.java to use an arraylist
    router.java uses an arraylist now - only problem is that it doesn't want
    to read the IP addresses properly.

--- Router.java
+++ Router.java
@@ -4,20 +4,19 @@ import java.util.*;
 import java.io.*;
 
 /*****************************************************************************************
- * // class Router 
- * // Purpose: This class represents a router 
- * Linda Crane,   CST8130
- * // data members:
- * //  routingTable - to hold up to MaxEntries routing table entries 
- * //  numEntries - number of entries currently in the table  
- * //  maxEntries - maximum number of entries  in table 
- * // methods: constructor 
- * // displayTable - displays results of "show ip route" command on device-ie entries in table 
- * // processPackets (Packet) - uses the parameter "packet" - processes it    
+ * // class Router // Purpose: This class represents a router Linda Crane,
+ * CST8130 // data members: // routingTable - to hold up to MaxEntries routing
+ * table entries // numEntries - number of entries currently in the table //
+ * maxEntries - maximum number of entries in table // methods: constructor //
+ * displayTable - displays results of "show ip route" command on device-ie
+ * entries in table // processPackets (Packet) - uses the parameter "packet" -
+ * processes it
  ***************************************************************************************/
 class Router {
-	protected RoutingTableEntry[] routingTable;
-	protected int numEntries;
+	protected ArrayList<RoutingTableEntry> routingTable;
+
+	// protected RoutingTableEntry[] routingTable;
+	// protected int numEntries;
 	protected int maxEntries;
 
 	public Router() {
@@ -25,20 +24,25 @@ class Router {
 		System.out.print("Enter number of entries maximum for array: ");
 		maxEntries = keyboard.nextInt();
 
-		routingTable = new RoutingTableEntry[maxEntries];
+		// routingTable = new RoutingTableEntry[maxEntries];
+		routingTable = new ArrayList<RoutingTableEntry>();
 		// check this worked***********************************************
-		numEntries = 0;
+		// numEntries = 0;
 
 	}
-	
-	
 
 	public void processPackets(Packet inPacket) {
 
 		boolean found = false;
 		String port = null;
-		for (int i = 0; i < numEntries; i++) {
-			port = routingTable[i].searchForPort(inPacket.getDestNetwork());
+		int[] testNum;
+		int[] compNum;
+		for (int i = 0; i < routingTable.size(); i++) {
+			if (routingTable.contains(inPacket.getDestNetwork())) {
+
+			}
+			port = String.valueOf(routingTable.indexOf(inPacket
+					.getDestNetwork()));
 			if (port != null) {
 				found = true;
 				break;
@@ -51,19 +55,53 @@ class Router {
 			addToTable = inPacket.processNotFoundPacket(port);
 
 		if (addToTable) {
-			if (numEntries >= maxEntries)
-				System.out.println("Table is full.........cannot add " + inPacket.getDestNetwork());
+			if (routingTable.size() >= maxEntries)
+				System.out.println("Table is full.........cannot add "
+						+ inPacket.getDestNetwork());
 			else {
-				routingTable[numEntries] = new RoutingTableEntry();
-				routingTable[numEntries++].addEntry(inPacket.getDestNetwork(),
-						inPacket.getPacketData());
+				if (routingTable.isEmpty()) {
+					routingTable.add(new RoutingTableEntry());
+					routingTable.get(0).addEntry(inPacket.getDestNetwork(),
+							inPacket.getPacketData());
+					// routingTable.add(inPacket.getDestNetwork(),
+					// inPacket.getPacketData());
+				} else {
+					for (int q = 0; q < routingTable.size(); q++) {
+						testNum = inPacket.getDestNetwork().address;
+						compNum = routingTable.get(q).destination.address;
+						if (testNum[0] < compNum[0]) {
+							routingTable.add(q, new RoutingTableEntry());
+							routingTable.get(q).addEntry(
+									inPacket.getDestNetwork(),
+									inPacket.getPacketData());
+						} else if (testNum[1] < compNum[1]) {
+							routingTable.add(q, new RoutingTableEntry());
+							routingTable.get(q).addEntry(
+									inPacket.getDestNetwork(),
+									inPacket.getPacketData());
+						} else if(testNum[2] < compNum[2]){
+							routingTable.add(q, new RoutingTableEntry());
+							routingTable.get(q).addEntry(
+									inPacket.getDestNetwork(),
+									inPacket.getPacketData());
+						} else if(testNum[3] < compNum[3]){
+							routingTable.add(q, new RoutingTableEntry());
+							routingTable.get(q).addEntry(
+									inPacket.getDestNetwork(),
+									inPacket.getPacketData());
+						}
+					}
+
+				}
+
 			}
 		}
 	}
 
 	public void displayTable() {
 		System.out.println("\nRouting table...\n");
-		for (int i = 0; i < numEntries; i++)
-			System.out.println(routingTable[i]);
+		for (int i = 0; i < routingTable.size(); i++)
+			System.out.println(routingTable.get(i));
 	}
+
 };
